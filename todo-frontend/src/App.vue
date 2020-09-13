747414c7a15eae661c276794a7228a281ea70c40
rkz1337 <booster2228@yandex.ru>
2020-09-13 20:31:37 +0300

    initial commit

---
+++ todo-frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Navbar />
+    <div class="container">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import M from "materialize-css";
+import Navbar from "@/components/Navbar";
+export default {
+  components: { Navbar },
+};
+</script>
+
+<style lang="scss">
+@import "~materialize-css/dist/css/materialize.min.css";
+body {
+  font-family: "Montserrat", "Segoe UI", sans-serif;
+}
+</style>
