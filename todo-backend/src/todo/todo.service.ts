87012a2d5c0ff22686dfe2d818052bd05b1d9320
rkz1337 <booster2228@yandex.ru>
2020-09-14 09:10:59 +0300

    Add Service for rest api

---
+++ todo-backend/src/todo/todo.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post } from './interfaces/post.interface';
+import { CreateTodoDTO } from './dto/create-todo.dto';
+
+@Injectable()
+export class TodoService {
+  constructor(@InjectModel('Todo') private readonly postModel: Model<Post>) {}
+
+  async getTodos(): Promise<Post[]> {
+    const todos = await this.postModel.find().exec();
+    return todos;
+  }
+
+  async getTodo(todoID): Promise<Post> {
+    const todo = await this.postModel.findById(todoID).exec();
+    return todo;
+  }
+
+  async addTodo(CreateTodoDTO: CreateTodoDTO): Promise<Post> {
+    const newTodo = await this.postModel(CreateTodoDTO);
+    return newTodo.save();
+  }
+
+  async editTodo(todoID, CreateTodoDTO: CreateTodoDTO): Promise<Post> {
+    const editedTodo = await this.postModel.findByIdAndUpdate(
+      todoID,
+      CreateTodoDTO,
+      { new: true },
+    );
+    return editedTodo;
+  }
+
+  async deleteTodo(todoID): Promise<any> {
+    const deleteTodo = await this.postModel.findByIdAndRemove(todoID);
+    return deleteTodo;
+  }
+}
